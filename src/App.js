ade96a70f5571ff4df31fa79f0550ef009e17c51
Takeddiney <y.takeddine@fullwaveagency.com>
2022-02-20 13:59:34 +0100

    Turn snakey interface into loading page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
   return (
     <div className="app">
-      <header className="app__header">
+      {loading ? <header className="app__header">
         <h1 className='app__title'>
-         Snakey
-         </h1>
-      </header>
+          Snakey
+        </h1>
+      </header> :
+        <header>
+          Snakey
+        </header>
+      }
     </div>
   );
 }
