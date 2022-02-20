28b31c3dea6bbd066d01a9cf02e128e39b723fd2
Takeddiney <y.takeddine@fullwaveagency.com>
2022-02-20 15:57:54 +0100

    Update site name & styles

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ function App() {
   }, [])
   return (
     <div className="app">
-      {loading ? <header className="app__header">
-        <h1 className='app__title'>
-          Snakey
-        </h1>
-      </header> :
-        <header>
-          Snakey
-        </header>
-      }
+      <header className="app__header">
+        {loading ?
+          <h1 className='app__title'>
+            SNAKEY
+          </h1>
+          :
+          <button>
+            START GAME
+          </button>
+        }
+      </header>
     </div>
   );
 }
